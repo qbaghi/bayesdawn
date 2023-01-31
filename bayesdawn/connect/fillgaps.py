403b5a6c2e8d9f21ebc1ce0f943089e664c95ba2
Eleonora Castelli <ell.castelli@gmail.com>
2023-01-31 11:45:38 -0500

    Remove unnecessary functions

--- bayesdawn/connect/fillgaps.py
+++ bayesdawn/connect/fillgaps.py
@@ -12,65 +12,13 @@ from cycler import cycler
 from ldc.lisa.noise import get_noise_model
 from bayesdawn import datamodel, psdmodel
 
-# Blackman-Harris function
-def BH92(M:int):
-    z = np.arange(0,M)*2*np.pi/M
-    return 0.35875 - 0.48829 * np.cos(z) + 0.14128 * np.cos(2*z) - 0.01168 * np.cos(3*z)
-
-# Blackman-Harris sliding window low-pass filter 
-def BH_lowpass(data, t_win=100,t_sam=5,fs=10):
-    """
-    Lowpass data convolving with a BH92 windowing function.
-    based on LTPDA BH_lowpass.m by E Castelli, 2018
-    translated to python by L Sala UniTrento, December 2021
-    modified by E Castelli, Sept 2022
-    
-    Parameters:
-    -----------
-        data:  
-            tuple of synchronously sampled time series
-        t_win: 
-            window length, controls cut frequency
-        t_sam: 
-            output sampling time
-        fs:    
-            input sampling frequency
-    
-    Returns:
-    --------
-        datalp:
-            lowpassed data
-
-
-    """
-    names = data.dtype.names[1:]
-    dt = 1/fs
-    step_win = np.intc(t_win*fs)
-    step_sam = np.intc(t_sam*fs)
-    assert t_sam>=dt, 'Watch out, do not upsample your data.'
-    assert np.isclose(t_sam*fs,int(t_sam*fs),rtol=1e-5), 'Downsampling time must be multiple of sampling time.'
-    assert np.isclose(t_win*fs,int(t_win*fs),rtol=1e-5), 'Windowing time must be multiple of sampling time.'
-    assert np.isclose(step_win/step_sam,int(step_win/step_sam),rtol=1e-5), 'Watch out, t_win must be multiple of t_sam.'
-    
-    dtarr = np.diff(data['t'])
-    assert np.isclose(dtarr[0],dt,rtol=1e-5), 'Aaargh, sampling frequency is not consistent with data.' #just check fs
-    assert np.allclose(dtarr,dt,rtol=1e-5), 'Aaargh, your data are not equally sampled in time.' #just check sampling time
-
-    BHfilt = BH92(step_win) #build filter
-    BHarea = np.sum(BHfilt)
-    BHfilt = BHfilt/BHarea
-    onearray = np.ones(step_win)/step_win
-
-    #apply filter convolving
-    outts = [np.convolve(data['t'],onearray,mode='valid')] #just a simple way to get times, computationally more expensive than linspace, but safer
-    for tdi in names:
-        outts += [np.convolve(data[tdi], BHfilt,  mode='valid')]
-    #downsample it
-    for i in range(len(outts)):
-        outts[i] = outts[i][::step_sam]
-    datalp = np.rec.fromarrays(outts, names = ['t', 'A', 'E', 'T'])
-    return datalp
-
+# Function to print all attributes of hdf5 file recursively
+def print_attrs(name, obj):
+    shift = name.count('/') * '    '
+    print(shift + name)
+    for key, val in obj.attrs.items():
+        print(shift + '    ' + f"{key}: {val}")
+        
 # Function to import LDC 2 data and convert them in more convenient format 
 def load_tdi_timeseries(fname, 
                         import_datasets = ['obs','clean','sky','noisefree'], 
